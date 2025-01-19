8e246816941392f7a08581d4f6ab16a2e6e9f551
caio f f <caiofkreitas@gmail.com>
2025-01-19 09:54:33 -0300

    Create file

---
+++ js/main2.js
@@ -0,0 +1,16 @@
+//null - ausência intencional de um valor
+var teste = null;
+
+//undefined - variável, apesar de declarada, não possui nenhum valor.
+var teste2;
+
+//teste no console.log()
+console.log(teste, teste2);
+
+//atribuindo valor...
+
+teste = 'Modificado valor para string e o próximo para inteiro...';
+teste2 = 23;
+
+//testando...
+console.log(teste, teste2);
