f8b94dc131dfbac0aa14c2307a819ce8c6bc7e27
caio f f <caiofkreitas@gmail.com>
2025-01-21 20:38:36 -0300

    Updates

--- js/main-9.js
+++ js/main-9.js
@@ -1,1 +1,28 @@
-var data = new Date();
+//15/01/2017
+
+var data1 = new Date(2017, 0, 15);
+
+//21/01/2025
+
+var data2 = new Date(2025, 0, 21); 
+
+document.write(data1.toString());
+document.write('<hr>');
+document.write(data2.toString());
+document.write('<hr>');
+document.write('<br>');
+
+document.write(data1.getTime());
+document.write('<hr>');
+document.write(data2.getTime());
+document.write('<hr>');
+
+
+var difMilissegs = data2.getTime() - data1.getTime();
+
+var difDias = difMilissegs/(1*24*60*60*1000);
+
+document.write(Math.ceil(difDias));
+
+
+
