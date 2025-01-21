b146f259012087ff8639d2c72b4d7fc3c3a6963e
caio f f <caiofkreitas@gmail.com>
2025-01-21 09:49:39 -0300

    Updates

--- js/main-5.js
+++ js/main-5.js
@@ -14,22 +14,29 @@ altConv = alt/100;
 IMC = peso/(altConv*altConv);    
 
 
-if (IMC < 16.0) {
-    classific = 'baixo peso muito grave';
-}else if(IMC >= 16.0 && IMC < 17.0){
-    classific = 'baixo peso grave';
-}else if(IMC >= 17.0 && IMC < 18.5){
-    classific = 'baixo peso';
-}else if(IMC >= 18.5 && IMC < 25.0){
-    classific = 'peso normal';
-}else if(IMC >= 25.0 && IMC < 30.0){
-    classific = 'sobrepeso';
-}else if(IMC >= 30.0 && IMC < 35.0){
-    classific = 'obesidade grau I';
-}else if(IMC >= 35.0 && IMC < 40.0){
-    classific = 'obesidade grau II';
-}else if(IMC >= 40.0){
-    classific = 'obesidade grau III';
+function classificaIMC(IMC) {
+    var classific;
+
+    if (IMC < 16.0) {
+        classific = 'baixo peso muito grave';
+    }else if(IMC >= 16.0 && IMC < 17.0){
+        classific = 'baixo peso grave';
+    }else if(IMC >= 17.0 && IMC < 18.5){
+        classific = 'baixo peso';
+    }else if(IMC >= 18.5 && IMC < 25.0){
+        classific = 'peso normal';
+    }else if(IMC >= 25.0 && IMC < 30.0){
+        classific = 'sobrepeso';
+    }else if(IMC >= 30.0 && IMC < 35.0){
+        classific = 'obesidade grau I';
+    }else if(IMC >= 35.0 && IMC < 40.0){
+        classific = 'obesidade grau II';
+    }else if(IMC >= 40.0){
+        classific = 'obesidade grau III';
+    }
+    return classific;
 }
+
+classific = classificaIMC(IMC);
 
 document.write(nome + ' possui índice de massa corporal igual a ' + IMC + ', sendo classificado como ' + classific);
