3587cfeb1c1946ec0f81df886fc891a1c6cd7fb0
caio f f <caiofkreitas@gmail.com>
2025-01-23 10:41:25 -0300

    Updates

--- js/main-10.js
+++ js/main-10.js
@@ -17,3 +17,37 @@ function acao4() {
 function acao5() {
     alert('a tecla foi pressionada');
 }
+
+console.log(document.getElementById('nome_usuario'));
+
+function distribuiCaractere() {
+    //selecionar o valor digitado e atribuir o valor digitado a uma variável
+    var caractere = document.getElementById('entrada').value;
+
+    //limpar o campo de digitação
+    document.getElementById('entrada').value = '';
+
+    //limpar espaços em branco antes e após o final da string digitada
+    caractere.trim();
+
+    //logica para definir se é número ou letra
+    switch (caractere) {
+        case '0':
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':    
+        case '6':
+        case '7':
+        case '8':
+        case '9':  
+            //adicionar caractere no campo de números
+            document.getElementById('numeros').value += caractere;
+            break;
+    
+        default:
+            document.getElementById('letras').value += caractere;
+            break;
+    }
+}
