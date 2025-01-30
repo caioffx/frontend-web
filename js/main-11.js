cfe05271418234d57d8289033ed4a33cd26c87e3
caio f f <caiofkreitas@gmail.com>
2025-01-30 10:50:39 -0300

    Updates

---
+++ js/main-11.js
@@ -0,0 +1,21 @@
+var lista_coisas = Array();
+
+lista_coisas[0] = Array('Banana', 'Maçã','Manga','Pera');
+
+lista_coisas[1] = [];
+
+lista_coisas[1][0] = 'José';
+lista_coisas[1][1] = 'Maria';
+lista_coisas[1][2] = 'Jesus';
+lista_coisas[1][3] = 'Francisco';
+
+lista_coisas['frutas'] = Array('Banana', 'Maçã','Manga','Pera');
+
+lista_coisas['pessoas'] = [];
+
+lista_coisas['pessoas'][0] = 'José';
+lista_coisas['pessoas'][1] = 'Maria';
+lista_coisas['pessoas'][2] = 'Jesus';
+lista_coisas['pessoas'][3] = 'Francisco';
+
+console.log(lista_coisas);
