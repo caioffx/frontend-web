3af633630b17aa653cd1a5e7940b50a9af7f65dd
caio f f <caiofkreitas@gmail.com>
2025-02-02 11:53:04 -0300

    Updates

--- js/addOrd.js
+++ js/addOrd.js
@@ -1,19 +1,34 @@
-var object = Array('Cadeira', 'Impressora', 'Garfo');
+var object = Array('cadeira', 'impressora', 'garfo');
 
 function recValue(Array) {
 
+    //cria uma variável newObj e atribui o valor digitado no campo de texto (resgatado pelo id html textField) a ela
     var newObj = document.getElementById('textField').value;
     console.log(newObj);
 
-    if(object.indexOf(newObj) !== -1){
-        console.log('String já existente...');
-        console.log(object);
+
+    if(newObj === ''){
+        console.log('Informe um valor válido.');
     }else{
-        object.push(newObj);
-        console.log(object);
+        //verifica se palavra já foi adicionada (case sensitive)
+        if(object.indexOf(newObj) !== -1){
+            console.log('String já existente...');
+            console.log(object);
+        }else{
+            //adiciona newObj no final do array object
+            object.push(newObj);
+            console.log(object);
+
+            //limpar o campo de texto
+            document.getElementById('textField').value = '';
+        }
     }
 
 
+}
 
-
+function orderValue(Array){
+    //ordena array object em ordem alfanumérica e case sensitive (primeiro maiúsculas e depois minúsculas)
+    object.sort();
+    console.log(object);
 }
